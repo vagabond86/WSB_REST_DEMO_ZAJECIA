8f8bfcac80f7285c10e015f95a7e5a5506a54a50
Kamil Górzyński <kamilgorzynski@gmail.com>
2023-03-11 10:06:39 +0100

    uzupełnienie kodu klasy Song stworzenie kontrolera z metodą pobierającą listę piosenek stworzenie pliku requests

--- src/main/java/wsb/rest/models/Song.java
+++ src/main/java/wsb/rest/models/Song.java
@@ -1,7 +1,13 @@
 package wsb.rest.models;
 
 import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 @AllArgsConstructor
 public class Song {
     private Long id;
